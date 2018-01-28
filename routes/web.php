f97e837f9e4acc4be36892a572470cddb00dee1a
Daniel Furmanov <fde711@gmail.com>
2018-01-29 00:12:15 +0300

    some css changes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/contacts', function() {
+    return '/contacts';
+})->name('contacts');
+
+Route::get('/articles', function() {
+    return '/articles';
+})->name('articles');
+
+Route::get('/books', function() {
+    return '/books';
+})->name('books');
+
+Route::get('/interviews', function() {
+    return '/interviews';
+})->name('interviews');
+
+Route::get('/reviews', function() {
+    return '/reviews';
+})->name('reviews');
+
